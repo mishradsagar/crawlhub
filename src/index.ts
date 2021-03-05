e28924b04ba2599c7b538abbeaaa3e46fcaa6043
Sagar Mishra <smishra@techracers.io>
2021-03-05 19:55:27 +0530

    feat(mongoose): integrated mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { connect } from 'mongoose';
 import config from './config';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -12,6 +13,19 @@ app.use((req, res) => res.status(404).json({ message: 'Not found'}));
 app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+try {
+    connect(config.get('mongoUrl'), {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      });
+}
+catch (err) {
+    console.log(err);
+    process.exit(1);
+}
 
 app.listen(config.get('port'), () => {
     console.log( `server started at http://localhost:${ config.get('port') }` );
