3aa9743e340e00756ebf81872260acdd891e9c36
Sagar Mishra <smishra@deqode.com>
2021-03-07 21:09:11 +0530

    refactor(restclient): refactored api calls and created Rest Client class

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,39 +1,38 @@
-import fetch from 'node-fetch';
-import config from '../../config'
-import User from './user.dto';
-import userModel from './user.model'
+import fetch from "node-fetch";
+import { RestClient } from "../../lib/restClient";
+import config from "../../config";
+import User from "./user.dto";
+import userModel from "./user.model";
 
 export const getUserRepositories = async (userHandle) => {
-  const route = `https://api.github.com/users/${userHandle}/repos`;
-  const options = {
-    method: 'GET',
+  const restClient = new RestClient("https://api.github.com", {
+    method: "GET",
     headers: {
-      'client_id': config.get('github.clientId'),
-      'client_secret': config.get('github.clientSecret')
-    }
-  }
-  const result = await fetch(route, options)
-  return result.json();
-}
+      client_id: config.get("github.clientId"),
+      client_secret: config.get("github.clientSecret"),
+    },
+  });
+
+  const {  body } = await restClient.callGet(`/users/${userHandle}/repos`);
+  return body;
+};
 
 export const getUserInfo = async (userHandle): Promise<User> => {
   let userInfo: User = await userModel.findOne({ login: userHandle });
 
   if (userInfo) {
     return userInfo;
-  }
-  else {
-    const route = `https://api.github.com/users/${userHandle}`;
-    const options = {
-      method: 'GET',
+  } else {
+    const restClient = new RestClient("https://api.github.com", {
+      method: "GET",
       headers: {
-        'client_id': config.get('github.clientId'),
-        'client_secret': config.get('github.clientSecret')
-      }
-    }
-    const response = await fetch(route, options);
-    const result: User = await response.json();
-    const user = await userModel.create(result);
-    return user; 
+        client_id: config.get("github.clientId"),
+        client_secret: config.get("github.clientSecret"),
+      },
+    });
+    
+    const {  body } = await restClient.callGet(`/users/${userHandle}`);
+    const user = await userModel.create(body);
+    return user;
   }
-}
+};
