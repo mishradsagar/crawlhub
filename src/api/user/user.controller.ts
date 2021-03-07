a33f18ae1888256fe8621f733804dc12a3ac92d9
Sagar Mishra <smishra@techracers.io>
2021-03-07 22:49:44 +0530

    refactor(errorhandler): added global error handler

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -5,14 +5,24 @@ import * as userService from './user.service';
 
 export const getUserRepositories = async (req: Request, res: Response, next: NextFunction) => {
   const { userHandle } = req.params;
-
-  const result = await userService.getUserRepositories(userHandle);
-  return res.status(200).json(result);
+  
+  try {
+    const result = await userService.getUserRepositories(userHandle);
+    return res.status(200).json(result);
+  }
+  catch (err) {
+    next(err);
+  }
 }
 
 export const getUserInfo = async (req: Request, res: Response, next: NextFunction) => {
   const { userHandle } = req.params;
 
-  const result: User = await userService.getUserInfo(userHandle);
-  return res.status(200).json(result);
+  try {
+    const result: User = await userService.getUserInfo(userHandle);
+    return res.status(200).json(result);
+  }
+  catch (err) {
+    next(err);
+  }
 }
