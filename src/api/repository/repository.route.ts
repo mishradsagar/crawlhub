6f92748b1387213f3d650c60345a814c3c932170
Sagar Mishra <smishra@deqode.com>
2021-03-08 13:18:27 +0530

    refactor(validation): added validation for search repository api

--- src/api/repository/repository.route.ts
+++ src/api/repository/repository.route.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { searchRepositories } from './repository.controller';
+import { repository } from './repository.validation';
 
 const router = Router();
 
-router.get('/', searchRepositories);
+router.get('/', [repository.repoName], searchRepositories);
 
 export default router;
